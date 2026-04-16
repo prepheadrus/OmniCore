a25a724c94c9901fd0b18ba75afb1fb580b84d6b
Ersen Kazar <83468174+prepheadrus@users.noreply.github.com>
2026-04-16 08:37:58 +0300

    Revert "feat: Bundle and BOM Engine with Recursive Inventory Reduction"

--- libs/core-domain/src/lib/core-domain.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { InventoryReductionService } from './services/inventory-reduction.service';
-
-@Module({
-  controllers: [],
-  providers: [InventoryReductionService],
-  exports: [InventoryReductionService],
-})
-export class CoreDomainModule {}
